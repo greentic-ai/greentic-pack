402fe9e1fb0252c9d4695c399946d3a69df1c95d
Maarten Ectors <maarten@greentic.ai>
2025-10-30 22:17:44 +0000

    trying to fix publish

--- crates/pack_component_template/src/lib.rs
+++ crates/pack_component_template/src/lib.rs
@@ -1,3 +1,3 @@
-pub const CARGO_TOML: &str = include_str!("../templates/Cargo.toml");
-pub const LIB_RS: &str = include_str!("../templates/lib.rs");
-pub const DATA_RS_PLACEHOLDER: &str = include_str!("../templates/data.rs");
+pub const CARGO_TOML: &str = include_str!("templates/template_Cargo.toml");
+pub const LIB_RS: &str = include_str!("templates/template_lib.rs");
+pub const DATA_RS_PLACEHOLDER: &str = include_str!("templates/template_data.rs");
